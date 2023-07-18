a2274662e0dbfc60fb1084d45078bd3eace044ff
Denmark <denmarkcanete@gmail.com>
2023-07-18 18:41:54 +0800

    standalone

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export', // comment this line if running npm run start | uncomment if running npm run deploy
+    standalone: true,
+}
+
+module.exports = nextConfig
